07380c7982d8dbb8670288dd5444456fd89ca62a
electrovir <electrovir@users.noreply.github.com>
2025-08-28 15:04:46 +0000

    [minor] ask to deploy before all deploys

--- src/git.ts
+++ src/git.ts
@@ -1,6 +1,6 @@
 import {assert, assertWrap, check} from '@augment-vir/assert';
-import {extractErrorMessage, log, logColors} from '@augment-vir/common';
-import {askQuestion} from '@augment-vir/node';
+import {log, logColors} from '@augment-vir/common';
+import {confirm} from '@inquirer/prompts';
 import {type DefaultLogFields, type ListLogLine, type SimpleGit} from 'simple-git';
 import {type DeployVirBranchConfig, type DeployVirRepoConfig} from './config.js';
 import {KnownError} from './known.error.js';
@@ -134,72 +134,61 @@ export async function pushDeploy(
         throw new KnownError('No commit diff: nothing to deploy!');
     }
 
-    try {
-        await git.push(remoteName, pushString);
+    /** Get commits that are on {@link toBranch} but not on {@link fromBranch}. */
+    const commitsBehind = (
+        await git.log([
+            `${remoteName}/${fromBranch}..${remoteName}/${toBranch}`,
+        ])
+    ).all;
 
-        return {
-            deployCommits: {
-                deployedCommits: commitsAhead,
-                overwrittenCommits: [],
-            },
-            branchStatus: {
-                before: beforeCommit,
-                after: afterCommit,
-            },
-        };
-    } catch (error) {
-        log.error(`Push failed: ${extractErrorMessage(error)}`);
+    const requiresForcePush = commitsBehind.length > 0;
 
-        /** Get commits that are on {@link toBranch} but not on {@link fromBranch}. */
-        const commitsBehind = (
-            await git.log([
-                `${remoteName}/${fromBranch}..${remoteName}/${toBranch}`,
-            ])
-        ).all;
-
-        if (commitsBehind.length > 0) {
-            log.info(`\nOnly on ${logColors.bold}${fromBranch}${logColors.reset}:`);
-            commitsAhead.forEach((commit, index) => {
-                log.faint(
-                    `    ${index + 1}. ${commit.hash.slice(0, 7)} (${commit.author_name}) - ${commit.message}`,
-                );
-            });
-
-            log.info(`\nOnly on ${logColors.bold}${toBranch}${logColors.reset}:`);
-            commitsBehind.forEach((commit, index) => {
-                log.faint(
-                    `    ${index + 1}. ${commit.hash.slice(0, 7)} (${commit.author_name}) - ${commit.message}`,
-                );
-            });
-        }
-
-        const shouldForcePush = await askQuestion(
-            `\nDo you want to force push ${logColors.bold}${fromBranch}${logColors.reset} to ${logColors.bold}${toBranch}${logColors.reset}?\n\n${logColors.warning}This will overwrite the commits only on ${logColors.bold}${toBranch}${logColors.normalWeight}.${logColors.reset} (y/N): `,
-            {
-                timeout: {
-                    hours: 2,
-                },
-            },
+    log.info(
+        `\n${requiresForcePush ? 'Only on' : 'Releasing from'} ${logColors.bold}${fromBranch}${logColors.reset}:`,
+    );
+    commitsAhead.forEach((commit, index) => {
+        log.faint(
+            `    ${index + 1}. ${commit.hash.slice(0, 7)} (${commit.author_name}) - ${commit.message}`,
         );
+    });
 
-        if (shouldForcePush.toLowerCase() === 'y' || shouldForcePush.toLowerCase() === 'yes') {
-            await git.push(remoteName, pushString, ['--force']);
-            log.warning(
-                `Force pushed ${logColors.bold}${fromBranch}${logColors.reset} to ${logColors.bold}${toBranch}${logColors.reset}.`,
+    if (requiresForcePush) {
+        log.info(`\nOnly on ${logColors.bold}${toBranch}${logColors.reset}:`);
+        commitsBehind.forEach((commit, index) => {
+            log.faint(
+                `    ${index + 1}. ${commit.hash.slice(0, 7)} (${commit.author_name}) - ${commit.message}`,
             );
-        } else {
-            throw new KnownError(`Deploy aborted.`);
-        }
-
-        return {
-            deployCommits: {
-                deployedCommits: commitsAhead,
-                overwrittenCommits: commitsBehind,
-            },
-            branchStatus: {
-                before: beforeCommit,
-                after: afterCommit,
-            },
-        };
+        });
     }
+
+    const shouldPush = await confirm({
+        message: requiresForcePush
+            ? `\n${logColors.warning}Do you want to force push ${logColors.bold}${fromBranch}${logColors.normalWeight} to ${logColors.bold}${toBranch}${logColors.normalWeight}?\n\nThis will overwrite the commits only on ${logColors.bold}${toBranch}${logColors.normalWeight}.${logColors.reset}?`
+            : 'Ready to deploy?',
+        default: false,
+    });
+
+    if (!shouldPush) {
+        throw new KnownError(`Deploy aborted.`);
+    }
+
+    if (requiresForcePush) {
+        await git.push(remoteName, pushString, ['--force']);
+        log.warning(
+            `Force pushed ${logColors.bold}${fromBranch}${logColors.normalWeight} to ${logColors.bold}${toBranch}${logColors.reset}.`,
+        );
+    } else {
+        await git.push(remoteName, pushString);
+    }
+
+    return {
+        deployCommits: {
+            deployedCommits: commitsAhead,
+            overwrittenCommits: commitsBehind,
+        },
+        branchStatus: {
+            before: beforeCommit,
+            after: afterCommit,
+        },
+    };
 }
